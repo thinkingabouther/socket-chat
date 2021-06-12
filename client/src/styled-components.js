d5bd35c695c4546259c73b1c5ce8a9907b2d5a50
a.neustroev <a.neustroev@xsolla.com>
2021-06-12 13:32:09 +0500

    deleted deprecated components

--- client/src/styled-components.js
+++ client/src/styled-components.js
@@ -1,86 +1,5 @@
 import styled from "styled-components";
 
-export const PeersVideoContainer = styled.div`
-  padding: 1rem;
-  display: flex;
-  height: 90vh;
-  width: 90vw;
-  margin: auto;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: space-around;
-`;
-
-export const SinglePeerVideoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background: white;
-`;
-
-export const VideoDescriptionContainer = styled.div`
-  background: white;
-  width: 100%;
-  text-align: center;
-`;
-
-export const VideoDescription = styled.span`
-  color: black;
-  text-align: center;
-  font-size: large;
-`;
-
-export const PeerVideoPlayer = styled.video`
-  height: 360px;
-  width: 640px;
-  object-fit: cover;
-`;
-
-export const UserVideo = styled.video`
-  height: 180px;
-  width: 320px;
-  object-fit: cover;
-`;
-
-export const UserVideoContainer = styled.div`
-  max-width: 30rem;
-  max-height: 30rem;
-  height: auto;
-  width: auto;
-  position: absolute;
-  top: 1rem;
-  left: 1rem;
-  z-index: 1;
-  display: flex;
-  flex-direction: column;
-`;
-
-export const ControlsContainer = styled.div`
-  position: absolute;
-  bottom: 0;
-  width: 100vw;
-  height: 5rem;
-  z-index: 1;
-  align-items: center;
-  justify-content: space-around;
-  display: flex;
-  background: white;
-`;
-
-export const RoomContainer = styled.div`
-  background: black;
-`;
-
-export const ControlsIconContainer = styled.span`
-  padding: 10px;
-  border-radius: 50%;
-  border: 2px solid black;
-  width: 4rem;
-  height: 4rem;
-  cursor: pointer;
-  display: flex;
-  background: lightgrey;
-`;
-
 export const ControlIcon = styled.img`
   object-fit: cover;
 `;
@@ -106,11 +25,6 @@ export const LogoutContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-`;
-
-export const JoinRoomContainer = styled.div`
-  display: flex;
-  flex-direction: column;
 `;
 
 export const HomeContainer = styled.div`
