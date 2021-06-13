b65ab8d93250e4aa2efe45975736d5cd2f75849e
thinkingabouther <thinkingabouther@users.noreply.github.com>
2021-06-13 18:09:54 +0000

    Prettified Code!

--- client/src/components/chat/MessageWrapper.js
+++ client/src/components/chat/MessageWrapper.js
@@ -13,34 +13,36 @@ const MessageWrapper = (props) => {
 
   if (props.message.url !== "") {
     return (
-    <a href={props.message.url} target="_blank">
-      <Message
-        type="image"
-        model={{
-          direction:
-            props.message.senderId === props.friendId ? "incoming" : "outgoing",
-          payload: {
-            src: props.message.url,
-            alt: "message_image",
-            width: "200px",
-          },
-        }}
-      >
-        <Message.Footer sentTime="19:05" />
-        <Avatar
-          src={
-            props.message.senderId === props.friendId
-              ? props.friendAvatarUrl
-              : props.context.user.picture
-          }
-          name={
-            props.message.senderId === props.friendId
-              ? props.friendName
-              : props.context.user.name
-          }
-        />
-      </Message>
-    </a>
+      <a href={props.message.url} target="_blank">
+        <Message
+          type="image"
+          model={{
+            direction:
+              props.message.senderId === props.friendId
+                ? "incoming"
+                : "outgoing",
+            payload: {
+              src: props.message.url,
+              alt: "message_image",
+              width: "200px",
+            },
+          }}
+        >
+          <Message.Footer sentTime="19:05" />
+          <Avatar
+            src={
+              props.message.senderId === props.friendId
+                ? props.friendAvatarUrl
+                : props.context.user.picture
+            }
+            name={
+              props.message.senderId === props.friendId
+                ? props.friendName
+                : props.context.user.name
+            }
+          />
+        </Message>
+      </a>
     );
   }
   return (
